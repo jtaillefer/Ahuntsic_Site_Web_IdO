e01e6c74f697f82aceca8e638aa75f503ba0975b
Jacques Taillefer <jacques.taillefer@collegeahuntsic.qc.ca>
2019-09-12 13:32:45 -0400

    init commit

---
+++ precache-manifest.3d2528bf776681f77e8a5c8238df8c93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe2e93cafd031bbc697cbe9484c6da22",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e51fe58ddb9159c33769",
+    "url": "/static/css/main.9383d537.chunk.css"
+  },
+  {
+    "revision": "50540956c4f890f47a6d",
+    "url": "/static/js/2.a8fe179c.chunk.js"
+  },
+  {
+    "revision": "e51fe58ddb9159c33769",
+    "url": "/static/js/main.39c5bd3d.chunk.js"
+  },
+  {
+    "revision": "963e368e2aa162bebce2",
+    "url": "/static/js/runtime~main.06df887e.js"
+  },
+  {
+    "revision": "29ac4edec5136925c58a68afe7a747df",
+    "url": "/static/media/noCamera.29ac4ede.svg"
+  },
+  {
+    "revision": "88a3fcb607405612f122ed346fb449f2",
+    "url": "/static/media/robot.88a3fcb6.svg"
+  }
+]);
